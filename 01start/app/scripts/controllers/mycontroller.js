0e905eaabb9712f3edd9cf4335742c093697b85a
刘放 <362512489@qq.com>
2020-07-16 09:47:01 +0800

    feat: init stafrt

---
+++ 01start/app/scripts/controllers/mycontroller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 01startApp.controller:MycontrollerCtrl
+ * @description
+ * # MycontrollerCtrl
+ * Controller of the 01startApp
+ */
+angular.module('01startApp')
+  .controller('MycontrollerCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # yeoman-demo
 demo for yeoman
+
+## 01start
+
+简单的yeoman生成demo
